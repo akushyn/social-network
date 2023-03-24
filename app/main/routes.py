d58cf4031dcd95a88515b05a99150d791d00fb07
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-25 00:23:54 +0700

    add about view

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from . import bp
-from flask import render_template
+from flask import render_template, flash
 
 
 @bp.route("/")
@@ -7,6 +7,10 @@ from flask import render_template
 def index():
     context = {
         "user": {"username": "akushyn"},
-        "title": "Hillel"
     }
     return render_template("index.html", **context)
+
+
+@bp.route('/about')
+def about():
+    return render_template("about.html")
