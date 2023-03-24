12e5ca74c35cb7fb0336ca86c3fab71427fda15f
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-24 09:30:07 +0700

    add register view function

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
 from app.auth import bp
 from flask import render_template, redirect, url_for
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 
 
 @bp.route("/login", methods=["GET", "POST"])
@@ -15,7 +15,16 @@ def login():
     # validate form on "POST" request
     if form.validate_on_submit():
         # redirect to home page
-        return redirect(url_for("index"))
+        return redirect(url_for("main.index"))
 
     # render 'login.html' template with passed form
     return render_template("auth/login.html", form=form)
+
+
+@bp.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for("main.index"))
+
+    return render_template("auth/register.html", form=form)
