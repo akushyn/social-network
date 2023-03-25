8ccb858b04dd1ff5aebabe00f5791c35f53125b0
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-25 23:42:32 +0700

    update forms, inherit login form

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -13,24 +13,28 @@ class LoginForm(FlaskForm):
     """
     Login form
     """
-    username = StringField("Username", validators=[validators.DataRequired()])
-    password = PasswordField("Password", validators=[validators.DataRequired()])
+    username = StringField("Username",validators=[validators.DataRequired(message="Username is required")])
+    password = PasswordField(
+        "Password",
+        validators=[
+            validators.DataRequired(message="Password is required"),
+            validators.Length(min=6, message="Min 6 length of password is required")
+        ]
+    )
     remember = BooleanField("Remember")
     submit = SubmitField("Log In")
 
 
-class RegisterForm(FlaskForm):
+class RegisterForm(LoginForm):
     """
     Register form
     """
-    username = StringField("Username", validators=[validators.DataRequired()])
-    email = EmailField("Email")
-    password = PasswordField("Password", validators=[validators.DataRequired()])
+    email = StringField("Email", validators=[validators.DataRequired(message="Email is required"), validators.Email()])
     confirm_password = PasswordField(
         "Confirm Password",
         validators=[
-            validators.DataRequired(),
-            validators.EqualTo("password")
+            validators.DataRequired(message="Confirm password is required"),
+            validators.EqualTo("password", message="Passwords should match")
         ]
     )
     submit = SubmitField("Register")
