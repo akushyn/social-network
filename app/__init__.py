b42e0b9dab7a12c5082d847c55a45d9dbf9016e9
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-25 23:54:26 +0700

    add context_processor

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+from flask_login import current_user
 
 
 def create_app():
@@ -21,6 +22,11 @@ def create_app():
     from .auth import bp as auth_bp
     app.register_blueprint(auth_bp)
 
+    @app.context_processor
+    def context_processor():
+        return dict(
+            current_user=current_user
+        )
     return app
 
 
