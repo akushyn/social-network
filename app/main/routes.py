64dbd1fa3cbde4f7dc1be27695f239f8844108ce
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-26 18:52:22 +0700

    update index route to insert and query all users

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,14 +1,59 @@
+from app import db
 from app.main import bp
 from flask import render_template
+
+from app.models import User
 
 
 @bp.route("/")
 @bp.route("/index")
 def index():
-    context = {
-        "user": {"username": "akushyn"},
-    }
-    return render_template("index.html", **context)
+
+    users_data = [
+        {
+            "username": "akushyn",
+            "email": "akushyn@gmail.com"
+        },
+        {
+            "username": "anton",
+            "email": "antonn@gmail.com"
+        },
+        {
+            "username": "denys",
+            "email": "denys@gmail.com"
+        },
+        {
+            "username": "tanya",
+            "email": "tanya@gmail.com"
+        },
+        {
+            "username": "igor",
+            "email": "igor@gmail.com"
+        },
+    ]
+
+    for u in users_data:
+        user = (
+            db.session.query(User)
+            .filter(
+                User.username == u.get('username'),
+                User.email == u.get('email')
+            ).first()
+        )
+        if user:
+            continue
+
+        user = User(
+            username=u.get('username'),
+            email=u.get('email')
+        )
+        db.session.add(user)
+    db.session.commit()
+
+    users_query = db.session.query(User)
+    users = users_query.all()
+    print(users)
+    return render_template("index.html", users=users)
 
 
 @bp.route('/about')
