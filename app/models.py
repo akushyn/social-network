5393b4d77845778ae145a09d347a120d31ae597f
Andrii Kushynskyi <akushyn@gmail.com>
2023-03-26 18:49:21 +0700

    add email to User model

--- app/models.py
+++ app/models.py
@@ -4,3 +4,7 @@ from app import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String, unique=True, index=True)
+    email = db.Column(db.String, unique=True, index=True)
+
+    def __repr__(self):
+        return f"{self.username}({self.email})"
