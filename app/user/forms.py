85714449752275d140b6f1b0f167df9ff7568654
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-05 01:01:38 +0700

    alembic migrtion to add linkedin, facebook

--- app/user/forms.py
+++ app/user/forms.py
@@ -3,7 +3,7 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import Length
 
 
-class EditProfileForm(FlaskForm):
+class ProfileForm(FlaskForm):
     first_name = StringField("First Name")
     last_name = StringField("Last Name")
     bio = TextAreaField('About me', validators=[Length(min=0, max=200)])
