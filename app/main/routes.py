b924f9c879b8036a96ef6bbe0a8b520c994df7e5
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-05 01:03:58 +0700

    implement blog view, update profile view

--- app/main/routes.py
+++ app/main/routes.py
@@ -2,17 +2,15 @@ from app import db
 from app.main import bp
 from flask import render_template
 
-from app.models import User
+from app.models import Post
 
 
 @bp.route("/")
 @bp.route("/index")
 def index():
 
-    users_query = db.session.query(User)
-    users = users_query.all()
-    print(users)
-    return render_template("index.html", users=users)
+    posts = db.session.query(Post).order_by(Post.created_at.desc()).all()
+    return render_template("index.html", posts=posts)
 
 
 @bp.route('/about')
--- app/user/routes.py
+++ app/user/routes.py
@@ -1,35 +1,48 @@
 from app import db
+from app.models import Post
+from app.post.forms import PostForm
 from app.user import bp
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_required
 
-from app.user.forms import EditProfileForm
+from app.user.forms import ProfileForm
 
 
-@bp.route("/profile")
+@bp.route("/blog")
+def blog():
+    """
+    Display user posts view
+    """
+    form = PostForm()
+    posts = (
+        db.session.query(Post)
+            .filter(
+            Post.author_id == current_user.id
+        )
+            .order_by(Post.created_at.desc())
+            .all()
+    )
+    return render_template("user/blog.html", posts=posts, form=form)
+
+
+@bp.route("/profile", methods=['GET', 'POST'])
 @login_required
 def profile():
-    posts = [
-        {'author': current_user, 'body': 'Test post #1'},
-        {'author': current_user, 'body': 'Test post #2'}
-    ]
-    return render_template("user/user.html", user=current_user, posts=posts)
-
-
-@bp.route('/profile/edit', methods=['GET', 'POST'])
-@login_required
-def edit_profile():
-    form = EditProfileForm()
+    form = ProfileForm()
     if form.validate_on_submit():
 
         current_user.profile.first_name = form.first_name.data
         current_user.profile.last_name = form.last_name.data
+        current_user.profile.linkedin = form.linkedin.data
+        current_user.profile.facebook = form.facebook.data
         current_user.profile.bio = form.bio.data
         db.session.commit()
         flash('Your changes have been saved.', category="success")
-        return redirect(url_for('user.edit_profile'))
+        return redirect(url_for('user.profile'))
     elif request.method == 'GET':
         form.first_name.data = current_user.profile.first_name
         form.last_name.data = current_user.profile.last_name
+        form.linkedin.data = current_user.profile.linkedin
+        form.facebook.data = current_user.profile.facebook
         form.bio.data = current_user.profile.bio
-    return render_template('user/edit_profile.html', title='Edit Profile', form=form)
+    return render_template('user/profile.html', user=current_user, form=form)
