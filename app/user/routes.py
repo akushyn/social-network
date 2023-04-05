81e6caca632e0be0145e97612c3604aca14a98ba
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-05 11:21:02 +0700

    update profile view, prevent ability to edit other user's profile

--- app/user/routes.py
+++ app/user/routes.py
@@ -1,5 +1,5 @@
 from app import db
-from app.models import Post
+from app.models import Post, User
 from app.post.forms import PostForm
 from app.user import bp
 from flask import render_template, flash, redirect, url_for, request
@@ -25,24 +25,25 @@ def blog():
     return render_template("user/blog.html", posts=posts, form=form)
 
 
-@bp.route("/profile", methods=['GET', 'POST'])
+@bp.route("/profile/<string:username>", methods=['GET', 'POST'])
 @login_required
-def profile():
+def profile(username):
+    user = db.session.query(User).filter(User.username == username).first_or_404()
     form = ProfileForm()
     if form.validate_on_submit():
 
-        current_user.profile.first_name = form.first_name.data
-        current_user.profile.last_name = form.last_name.data
-        current_user.profile.linkedin = form.linkedin.data
-        current_user.profile.facebook = form.facebook.data
-        current_user.profile.bio = form.bio.data
+        user.profile.first_name = form.first_name.data
+        user.profile.last_name = form.last_name.data
+        user.profile.linkedin = form.linkedin.data
+        user.profile.facebook = form.facebook.data
+        user.profile.bio = form.bio.data
         db.session.commit()
         flash('Your changes have been saved.', category="success")
-        return redirect(url_for('user.profile'))
+        return redirect(url_for('user.profile', username=user.username))
     elif request.method == 'GET':
-        form.first_name.data = current_user.profile.first_name
-        form.last_name.data = current_user.profile.last_name
-        form.linkedin.data = current_user.profile.linkedin
-        form.facebook.data = current_user.profile.facebook
-        form.bio.data = current_user.profile.bio
-    return render_template('user/profile.html', user=current_user, form=form)
+        form.first_name.data = user.profile.first_name
+        form.last_name.data = user.profile.last_name
+        form.linkedin.data = user.profile.linkedin
+        form.facebook.data = user.profile.facebook
+        form.bio.data = user.profile.bio
+    return render_template('user/profile.html', user=user, form=form)
