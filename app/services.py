a8f7af2feb4ee4b640bda8d3a588d487d352a36e
Andrii Kushynskyi <akushyn@gmail.com>
2023-04-06 02:21:17 +0700

    fix user service create func

--- app/services.py
+++ app/services.py
@@ -13,9 +13,9 @@ class UserService:
         user = db.session.query(User).filter(User.username == username).first_or_404()
         return user
 
-    def create(self, data):
-        user = User(username=data.get('username'), email=data.get('email'))
-        user.set_password(data.get('password'))
+    def create(self, **kwargs):
+        user = User(username=kwargs.get('username'), email=kwargs.get('email'))
+        user.set_password(kwargs.get('password'))
 
         db.session.add(user)
         db.session.commit()
